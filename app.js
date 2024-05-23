bd32a69a70c50f25a017c188f28ba9b6ac37e1af
Kukac194 <ivan.kukucka194@gmail.com>
2024-05-23 21:09:41 +0200

    Add app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict'
+
+//Varijable
+
+const ime = 'Ivan';
+const prezime = 'Kukučka';
+
+//Funkcija
+console.log(`${ime} ${prezime}`);
+const main = function() {
+
+    console.log('Funkcija');
+}
+main();
